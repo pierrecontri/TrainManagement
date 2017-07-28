87af55bd497a42953309821ae4956a06b9373a36
Pierre Contri <pierrecontri@outlook.com>
2017-07-28 22:22:00 +0200

    Append configuration file to escape the HTML editing file

---
+++ UI/scripts/trainManagement.config.js
@@ -0,0 +1,7 @@
+// declare the config object into the document
+
+document.configuration = {
+  'switchsContainer_1': { 'type': 'switchsContainer', 'title': 'Main Track' },
+  'switchsContainer_2': { 'type': 'switchsContainer', 'title': 'Spur Track' },
+  'switchsContainer_3': { 'type': 'switchsContainer permanentSwitch', 'title': 'On/Off Track' }
+};
