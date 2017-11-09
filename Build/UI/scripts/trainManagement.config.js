eba3faf315d690a2b15842dfb1f95680e314fbb0
Pierre Contri <pierrecontri@outlook.com>
2017-11-09 20:37:26 +0100

    First built version

---
+++ Build/UI/scripts/trainManagement.config.js
@@ -0,0 +1,7 @@
+// declare the config object into the document
+
+document.configuration = {
+  'switchsContainer_1': { 'type': 'bistableSwitch switchsContainer', 'title': 'Main Track' },
+  'switchsContainer_2': { 'type': 'bistableSwitch switchsContainer', 'title': 'Spur Track' },
+  'switchsContainer_3': { 'type': 'monostableSwitch switchsContainer permanentSwitch', 'title': 'On/Off Track' }
+};
