4ad31d0e83252d59e664fba8c9c95171a14ef2c0
Pierre Contri <pierrecontri@outlook.com>
2017-11-10 21:11:20 +0100

    Don not forget the Linux part

--- TrainManagementWebServer.sh
+++ TrainManagementWebServer.sh
@@ -1,3 +1,4 @@
-#python3 "./src/TrainManagementWebServer.py" ElectronicControler.DummyControler
-#python3 "./src/TrainManagementWebServer.py" ElectronicControler.PiControler
-python "./src/TrainManagementWebServer.py" ElectronicControler.RSArduinoControler COMPORT:/dev/tty4
+# python3 "./src/TrainManagementWebServer.py" ElectronicControler.DummyControler
+# python3 "./src/TrainManagementWebServer.py" ElectronicControler.PiControler
+# python "./src/TrainManagementWebServer.py" ElectronicControler.RSArduinoControler COMPORT:/dev/tty4
+python "./Build/TrainManagementWebServer.pyc"
