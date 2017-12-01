8c40d42c40e1c6e6d1a05015b780bee14c489aba
Pierre Contri <pierrecontri@outlook.com>
2017-12-01 14:53:21 +0100

    append the html page to the starting bat in standalone paltform

--- TrainManagementWebServer.bat
+++ TrainManagementWebServer.bat
@@ -1,4 +1,5 @@
 echo off
+".\Build\UI\TrainManagement.html"
 REM python ".\src\TrainManagementWebServer.py" ElectronicControler.DummyControler
 REM python ".\src\TrainManagementWebServer.py" ElectronicControler.RSArduinoControler COMPORT:COM8
 python ".\Build\TrainManagementWebServer.pyc"
